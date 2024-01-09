8828d4d81882dcdd749681582a32d949c4eca2dc
ELAND\zhongqilin <z0925955648@gmail.com>
2024-01-09 15:37:59 +0800

    first commit

---
+++ src/ageClassification.ts
@@ -0,0 +1,26 @@
+/**
+ * 任務：實作一個函式 `classifyAge`，根據年齡進行分類。
+ *
+ * 範例：
+ * classifyAge(10) 應該回傳 "Child"
+ * classifyAge(15) 應該回傳 "Teenager"
+ * classifyAge(30) 應該回傳 "Adult"
+ * classifyAge(70) 應該回傳 "Senior"
+ *
+ * @param age - 一個需要被分類的年齡
+ * @returns - 回傳年齡的分類結果
+ */
+export function classifyAge(age: number): string {
+    // 請在此處寫下你的程式碼
+    if (typeof age === 'number') {
+
+        if (age <=)
+    }
+    if (age < 0 || age > 100) {
+        return '請輸入合理範圍'
+    }
+    if (age >= 10) {
+        return 'Child';
+    }
+    return ''
+}
